397b9fe659e918baf9879416b0f4fef187be3eb3
Tanim reja <tanim2reja@gmail.com>
2016-10-05 12:17:42 +0600

    add camera
    Using intent.

--- app/src/main/java/com/tutorial/jolpai/camera/MainActivity.java
+++ app/src/main/java/com/tutorial/jolpai/camera/MainActivity.java
@@ -2,27 +2,51 @@ package com.tutorial.jolpai.camera;
 
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     static Camera camera = null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public ImageView mImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = (ImageView)findViewById(R.id.imageView);
 
-        isCameraUsebyApp();
+       // isCameraUsebyApp();
+        dispatchTakePictureIntent();
 
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
     public boolean isCameraUsebyApp() {
 
         try {
